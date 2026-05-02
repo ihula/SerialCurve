9b281caa713b19795532cc9aeca83a4bde1fed57
hula <ihula123@outlook.com>
2026-05-03 05:49:58 +0800

    改正ubuntu下没有标题栏

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import sys
+# 解决 Ubuntu Wayland 无标题栏、QT 报错问题
+import os
+# 自动判断系统：只有 Linux(Ubuntu) 才启用 XCB 修复
+if sys.platform.startswith("linux"):
+    os.environ["QT_QPA_PLATFORM"] = "xcb"
+    os.environ["EGL_PLATFORM"] = "x11"
+
 from PyQt6.QtWidgets import QApplication
 from mainwindow import MainWindow
 
